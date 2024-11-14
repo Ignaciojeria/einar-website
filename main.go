1466c86d28b27bd2796f18c7b4dfa4932acd437a
ignaciojeria <ignaciovl.j@gmail.com>
2024-11-13 23:31:15 -0300

    projectName and environment hardcoded

--- main.go
+++ main.go
@@ -7,9 +7,10 @@ import (
 	"log"
 	"os"
 
-	ioc "github.com/Ignaciojeria/einar-ioc/v2"
-	_ "einar-website-api/app/shared/infrastructure/serverwrapper"
 	_ "einar-website-api/app/shared/infrastructure/healthcheck"
+	_ "einar-website-api/app/shared/infrastructure/serverwrapper"
+
+	ioc "github.com/Ignaciojeria/einar-ioc/v2"
 )
 
 //go:embed .version
@@ -17,6 +18,8 @@ var version string
 
 func main() {
 	os.Setenv(constants.Version, version)
+	os.Setenv("PROJECT_NAME", "einar-website-api")
+	os.Setenv("ENVIRONMENT", "production")
 	if err := ioc.LoadDependencies(); err != nil {
 		log.Fatal(err)
 	}
